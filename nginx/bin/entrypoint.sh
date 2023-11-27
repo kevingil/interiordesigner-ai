12dc0a08f46e450e50c6b4a2c9335baa0213cbe6
kevingil <kevin_gil@icloud.com>
2023-11-26 16:37:22 -0800

    update compose

--- nginx/bin/entrypoint.sh
+++ nginx/bin/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 certbot certonly --standalone -d $1 --email $2 -n --agree-tos --expand
+cat /var/log/letsencrypt/letsencrypt.log
 /usr/sbin/crond -f &
 /usr/sbin/nginx -g "daemon off;"  # this is run in the foreground, so need to be last
-cat /var/log/letsencrypt/letsencrypt.log
+
