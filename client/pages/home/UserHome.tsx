73353c9300d6179f02e37e3ed1bd2e5ee05ad752
kevingil <kevin_gil@icloud.com>
2023-11-26 20:30:24 -0800

    update error from dev notifs

--- client/pages/home/UserHome.tsx
+++ client/pages/home/UserHome.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 const UserHome: React.FC = () => {
   return (
     <div className="w-full rounded-xl p-4 bg-stone-900/90">
       <h1 className="text-4xl font-bold mb-8">Home Dashboard</h1>
-      <a href="/create" className="text-blue-500 underline mb-4">New Idea</a>
+      <Link href="/create" className="text-blue-500 underline mb-4">New Idea</Link>
     </div>
   );
 };
