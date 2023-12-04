c39699117547af927d12c4242d3cf7cb48cd0dca
kevingil <kevin_gil@icloud.com>
2023-12-03 23:32:58 -0800

    Del old files

--- renew_cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-certbot certonly --standalone -d $1 --email $2 -n --agree-tos --expand
-/usr/sbin/crond -f &
-/usr/sbin/nginx -g "daemon off;"  # this is run in the foreground, so need to be last
